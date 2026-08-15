41d18a1e300e1de2798f16010560bee0fec45e11
mohammedmasoud <42085679+mohammedmasoud@users.noreply.github.com>
2026-08-16 00:57:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -162,7 +162,7 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  root: path.resolve(import.meta.dirname, "client"),
+  root: import.meta.dirname,
   publicDir: path.resolve(import.meta.dirname, "client", "public"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
